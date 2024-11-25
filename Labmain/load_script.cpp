a1b6d7429ea57afda90cbe7c9e1cd27309172c5c
DaniSoria13 <dgarcsor@myuax.com>
2024-11-25 18:28:20 +0100

    creamos la etructura colorconsole

---
+++ Labmain/load_script.cpp
@@ -0,0 +1,3 @@
+//
+// Created by User on 25/11/2024.
+//
---
+++ Labmain/load_script.h
@@ -0,0 +1,18 @@
+//
+// Created by User on 25/11/2024.
+//
+
+#ifndef LOAD_SCRIPT_H
+#define LOAD_SCRIPT_H
+
+struct ColorConsole
+{
+  public:
+    static constexpr auto fg_blue = "\033[34m";
+    public:
+    static constexpr auto bg_white = "\033[47m";
+};
+
+
+
+#endif //LOAD_SCRIPT_H
