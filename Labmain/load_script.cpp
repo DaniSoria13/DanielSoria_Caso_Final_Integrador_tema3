a73baf7cc3b72b54d64c4a76e3395be781088430
DaniSoria13 <dgarcsor@myuax.com>
2024-11-25 18:30:45 +0100

    Inicializamos el consolebox

--- Labmain/load_script.cpp
+++ Labmain/load_script.cpp
@@ -1,3 +1,9 @@
-//
-// Created by User on 25/11/2024.
-//
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include "load_script.h"
+
+using namespace std;
+
+ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
+
